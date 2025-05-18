4e2da09c54b7f9aa8832f2261f3343455a21eaeb
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-05-18 17:26:54 +0100

    Configuração do alias para resolver o problema dos caminhos de imagens nos arquivos styled components

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'   // <== Importar o path
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  /*server: {
-    hmr: {
-      overlay: false
-    }
-  }*/
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
