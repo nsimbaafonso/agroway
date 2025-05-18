7a03a4e0295463f9cb25fdc8fb7bc2b3193927bc
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-05-18 18:16:15 +0100

    Atualização básica

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
 import Blog from './pages/Blog';
@@ -8,10 +12,20 @@ import Post from './pages/Post';
 import NotFound from './pages/NotFound';
 import Teste from './pages/Teste';
 import DashboardHome from './dashboard/DashboardHome';
-//import './assets/css/style.css'; // estilo global
+// import './assets/css/style.css'; // estilo global
 import GlobalStyle from './assets/css/GlobalStyle';
 
 function App() {
+  // useEffect é um hook que roda após o componente ser montado
+  useEffect(() => {
+    // Inicializa a biblioteca AOS (Animate On Scroll)
+    AOS.init({
+      duration: 1000, // Define a duração das animações em milissegundos (1 segundo)
+      once: false,    // Permite que a animação ocorra todas as vezes que o elemento entrar na tela
+      //mirror: true,   // Faz a animação acontecer também ao rolar para cima
+    });
+  }, []); // O array vazio faz com que o useEffect rode apenas uma vez, quando o componente for carregado
+
   return (
     <Router>
       <GlobalStyle />
