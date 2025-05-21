5684cbc04fefd87ba5cc4b568630b7a97c11aae5
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-05-21 12:22:59 +0100

    Configuração do talwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'   // <== Importar o path
+import path from 'node:path'   // <== Importar o path
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),  tailwindcss(),],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
