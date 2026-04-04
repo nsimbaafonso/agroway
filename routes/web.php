ef3d741ff84b11d98fa6fbacad2eb8ef93c60673
Nsimba Afonso <nsimbaafonso0@gmail.com>
2026-04-04 17:36:19 +0100

    Rota de produtos adicionada

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use App\Http\Controllers\SiteController;
 
 //Rotas do site
 Route::get('/', [SiteController::class, 'home'])->name('home');
+Route::get('/produtos', [SiteController::class, 'produtos'])->name('produtos');
