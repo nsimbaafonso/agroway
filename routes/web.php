5c9ec540797e8da05edd8940ca3705b56fc0aa8c
Nsimba Afonso <nsimbaafonso0@gmail.com>
2026-04-06 09:45:03 +0100

    Rotas da página sobre e contato do site adicionadas

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use App\Http\Controllers\SiteController;
 
 //Rotas do site
 Route::get('/', [SiteController::class, 'home'])->name('home');
+Route::get('/sobre', [SiteController::class, 'sobre'])->name('sobre');
+Route::get('/contato', [SiteController::class, 'contato'])->name('contato');
 Route::get('/produtos', [SiteController::class, 'produtos'])->name('produtos');
 Route::get('/produtos/detalhes', [SiteController::class, 'detalhesProduto'])->name('detalhes-produto');
 Route::get('/carrinho', [SiteController::class, 'carrinho'])->name('carrinho');
+Route::get('/checkout', [SiteController::class, 'checkout'])->name('checkout');
+
+//Rotas de login
