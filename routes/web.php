a4fa0a0ccad07a2aa59a76f57459b3bb39cf186c
Nsimba Afonso <nsimbaafonso0@gmail.com>
2026-04-06 08:39:45 +0100

    Rotas de produtos e detalhes do produto adicionados

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,5 @@ use App\Http\Controllers\SiteController;
 //Rotas do site
 Route::get('/', [SiteController::class, 'home'])->name('home');
 Route::get('/produtos', [SiteController::class, 'produtos'])->name('produtos');
+Route::get('/produtos/detalhes', [SiteController::class, 'detalhesProduto'])->name('detalhes-produto');
+Route::get('/carrinho', [SiteController::class, 'carrinho'])->name('carrinho');
